688580587f28ee649b90df4aa37db5fd1793e87e
Francesco Limpias <francescolimpias@gmail.com>
2023-02-21 15:34:38 +0100

    Created foreign keys migration

---
+++ database/migrations/9999_02_21_112606_add_foreign_keys.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        Schema::table("movies", function(Blueprint $table){
+
+            $table->foreignId("genre_id")->constrained();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+        Schema::table("movies", function(Blueprint $table){
+
+            $table->dropForeign("movies_genre_id_foreign");
+            $table->dropColumn("genre_id");
+            
+        });
+    }
+};
