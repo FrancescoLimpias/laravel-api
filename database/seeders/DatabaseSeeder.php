5f1a1fc621e97ffe944a42bd72be169051f04da7
Francesco Limpias <francescolimpias@gmail.com>
2023-02-21 16:11:11 +0100

    Added Tag seeding system

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            GenreSeeder::class
+            GenreSeeder::class,
+            TagSeeder::class
         ]);
     }
 }
--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -2,11 +2,45 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class TagSeeder extends Seeder
 {
+
+    const tags_list = [
+        "movies",
+        "movie",
+        "film",
+        "cinema",
+        "films",
+        "hollywood",
+        "actor",
+        "love",
+        "s",
+        "art",
+        "cinematography",
+        "actress",
+        "netflix",
+        "moviescenes",
+        "music",
+        "filmmaking",
+        "horror",
+        "instagood",
+        "bollywood",
+        "movienight",
+        "instagram",
+        "photography",
+        "comedy",
+        "cinephile",
+        "cine",
+        "tv",
+        "director",
+        "horrormovies",
+        "drama",
+        "filmmaker"
+    ];
     /**
      * Run the database seeds.
      *
@@ -14,6 +48,11 @@ class TagSeeder extends Seeder
      */
     public function run()
     {
-        //
+        foreach (TagSeeder::tags_list as $tag) {
+            $newTag = new Tag();
+            $newTag["name"] = $tag;
+            $newTag["description"] = fake()->text(255);
+            $newTag->save();
+        }
     }
 }
