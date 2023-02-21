4aed93c4419e90d6428e926504289e84f9e414fb
Francesco Limpias <francescolimpias@gmail.com>
2023-02-21 16:08:37 +0100

    Added Genre seeding system

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -2,11 +2,40 @@
 
 namespace Database\Seeders;
 
+use App\Models\Genre;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class GenreSeeder extends Seeder
 {
+
+    const genre_list = [
+        "Action",
+        "Adventure",
+        "Animation",
+        "Biography",
+        "Comedy",
+        "Crime",
+        "Documentary",
+        "Drama",
+        "Family",
+        "Fantasy",
+        "Film Noir",
+        "History",
+        "Horror",
+        "Music",
+        "Musical",
+        "Mystery",
+        "Romance",
+        "Sci-Fi",
+        "Short",
+        "Sport",
+        "Superhero",
+        "Thriller",
+        "War",
+        "Western"
+    ];
+
     /**
      * Run the database seeds.
      *
@@ -14,6 +43,11 @@ class GenreSeeder extends Seeder
      */
     public function run()
     {
-        //
+        foreach (GenreSeeder::genre_list as $genre) {
+            $newGenre = new Genre();
+            $newGenre["name"] = $genre;
+            $newGenre["description"] = fake()->text(255);
+            $newGenre->save();
+        }
     }
 }
