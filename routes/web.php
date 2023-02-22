2f8dcd53a3c7a7f64ee7e2be66d11dc8f9704ca4
Francesco Limpias <francescolimpias@gmail.com>
2023-02-22 07:03:20 +0100

    Added required routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Landing (redirect -> home)
+Route::redirect('/', 'home');
+
+// Indexing routes
+Route::get('home', [MovieController::class, "indexByGenre"]);
+Route::get('home/movie', [MovieController::class, "index"]);
+
+// Controller routes
+Route::resource('movie', MovieController::class);
